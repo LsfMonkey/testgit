bec772e25832750470dcc1412936aae832f822ed
liusf <liusf@fenbi.com>
2019-05-18 14:29:50 +0800

    add git test

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,13 @@
+/**
+ * @(#)GitTest.java, May 18, 2019.
+ * <p>
+ * Copyright 2019 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package PACKAGE_NAME;
+
+/**
+ * @author liushuaifei
+ */
+public class GitTest {
+}
