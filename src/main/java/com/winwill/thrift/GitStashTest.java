011fc949859328a7a605485fa793065ee64110e3
liusf <liusf@fenbi.com>
2019-05-18 15:36:24 +0800

    OK

---
+++ src/main/java/com/winwill/thrift/GitStashTest.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)GitStashTest.java, May 18, 2019.
+ * <p>
+ * Copyright 2019 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.winwill.thrift;
+
+/**
+ * @author liushuaifei
+ */
+public class GitStashTest {
+    public static void main(String[] args) {
+        System.out.println("Git Stash before must add");
+    }
+}
